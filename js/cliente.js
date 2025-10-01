ce502408bb7b23c8e284336fd1388c94d701de34
sebitabravo <sebastianalejandrobravocampos@gmail.com>
2025-10-01 04:35:31 -0300

    cambio de api de proyecto

--- js/cliente.js
+++ js/cliente.js
@@ -1,110 +1,109 @@
 // Variables
-var g_id_cliente = "";
+var g_id_cliente = '';
 
 // Funcion Agregar
 function agregarCliente() {
-    // Variables
-    var id_cliente = document.getElementById("txt_id_cliente").value;
-    var dv = document.getElementById("txt_dv").value;
-    var nombres = document.getElementById("txt_nombre").value;
-    var apellidos = document.getElementById("txt_apellido").value;
-    var email = document.getElementById("txt_email").value;
-    var celular = document.getElementById("txt_celular").value;
-    // Validaciones
-    // Validar id_cliente
-    if (id_cliente == "") {
-        mostrarAlerta("Error Id Cliente: Campo obligatorio");
-        return;
-    }
-    // Validar dv
-    const dvValido = /^[0-9kK]$/;
-    if (dv == "") {
-        mostrarAlerta("Error Dv: Campo obligatorio");
-        return;
-    }
-    if (!dvValido.test(dv)) {
-        mostrarAlerta("Error Dv: Número del 0 al 9 o la letra k");
-        return;
-    }
-    // Validar nombres
-    if (nombres == "") {
-        mostrarAlerta("Error Nombres: Campo obligatorio");
-        return;
-    }
-    // Validar apellidos
-    if (apellidos == "") {
-        mostrarAlerta("Error Apellidos: Campo obligatorio");
-        return;
-    }
-    // Validar email
-    if (email == "") {
-        mostrarAlerta("Error Email: Campo obligatorio");
-        return;
-    }
-    // Validar celular
-    if (celular == "") {
-        mostrarAlerta("Error Celular: Campo obligatorio");
-        return;
-    }
-    // Agreganis api cliente
-    const myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-    // Variable para formatear zona horaria
-    var fechaHoraActual = obtenerFechaHora();
-    // continuamos agregarndo la api
-    const raw = JSON.stringify({
-        "id_cliente": id_cliente,
-        "dv": dv,
-        "nombres": nombres,
-        "apellidos": apellidos,
-        "email": email,
-        "celular": celular,
-        "fecha_registro": fechaHoraActual
-    });
-    // metodo post para agregar cliente
-    const requestOptions = {
-        method: 'POST',
-        headers: myHeaders,
-        body: raw,
-        redirect: 'follow'
-    };
-    fetch("http://144.126.210.74:8080/api/cliente", requestOptions)
-    .then((response)=>{
-        if(response.status == 200){
-            location.href = "listar.html";
-        }
-        if(response.status == 400){
-            mostrarAlerta("El cliente ya existe");
-        }
-    })
-    .then(result => console.log(result))
-    .catch((error) => console.error(error));
+	// Variables
+	var id_cliente = document.getElementById('txt_id_cliente').value;
+	var dv = document.getElementById('txt_dv').value;
+	var nombres = document.getElementById('txt_nombre').value;
+	var apellidos = document.getElementById('txt_apellido').value;
+	var email = document.getElementById('txt_email').value;
+	var celular = document.getElementById('txt_celular').value;
+	// Validaciones
+	// Validar id_cliente
+	if (id_cliente == '') {
+		mostrarAlerta('Error Id Cliente: Campo obligatorio');
+		return;
+	}
+	// Validar dv
+	const dvValido = /^[0-9kK]$/;
+	if (dv == '') {
+		mostrarAlerta('Error Dv: Campo obligatorio');
+		return;
+	}
+	if (!dvValido.test(dv)) {
+		mostrarAlerta('Error Dv: Número del 0 al 9 o la letra k');
+		return;
+	}
+	// Validar nombres
+	if (nombres == '') {
+		mostrarAlerta('Error Nombres: Campo obligatorio');
+		return;
+	}
+	// Validar apellidos
+	if (apellidos == '') {
+		mostrarAlerta('Error Apellidos: Campo obligatorio');
+		return;
+	}
+	// Validar email
+	if (email == '') {
+		mostrarAlerta('Error Email: Campo obligatorio');
+		return;
+	}
+	// Validar celular
+	if (celular == '') {
+		mostrarAlerta('Error Celular: Campo obligatorio');
+		return;
+	}
+	// Agreganis api cliente
+	const myHeaders = new Headers();
+	myHeaders.append('Content-Type', 'application/json');
+	// Variable para formatear zona horaria
+	var fechaHoraActual = obtenerFechaHora();
+	// continuamos agregarndo la api
+	const raw = JSON.stringify({
+		id_cliente: id_cliente,
+		dv: dv,
+		nombres: nombres,
+		apellidos: apellidos,
+		email: email,
+		celular: celular,
+		fecha_registro: fechaHoraActual
+	});
+	// metodo post para agregar cliente
+	const requestOptions = {
+		method: 'POST',
+		headers: myHeaders,
+		body: raw,
+		redirect: 'follow'
+	};
+	fetch('https://crm-react.sbravo.app/api/cliente', requestOptions)
+		.then((response) => {
+			if (response.status == 200) {
+				location.href = 'listar.html';
+			}
+			if (response.status == 400) {
+				mostrarAlerta('El cliente ya existe');
+			}
+		})
+		.then((result) => console.log(result))
+		.catch((error) => console.error(error));
 }
 
 // Funcion Listar
-function listarCliente(){
-    const myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-    // metodo get para listar cliente
-    var requestOptions = {
-        method: 'GET',
-        redirect: 'follow'
-    };
-    fetch("http://144.126.210.74:8080/api/cliente?_size=200", requestOptions)
-    .then(response => response.json())
-    .then((json) => {
-        json.forEach(completarFila);
-        $('#tbl_cliente').DataTable();
-    })
-    .then(result => console.log(result))
-    .catch((error) => console.error(error));
+function listarCliente() {
+	const myHeaders = new Headers();
+	myHeaders.append('Content-Type', 'application/json');
+	// metodo get para listar cliente
+	var requestOptions = {
+		method: 'GET',
+		redirect: 'follow'
+	};
+	fetch('https://crm-react.sbravo.app/api/cliente?_size=200', requestOptions)
+		.then((response) => response.json())
+		.then((json) => {
+			json.forEach(completarFila);
+			$('#tbl_cliente').DataTable();
+		})
+		.then((result) => console.log(result))
+		.catch((error) => console.error(error));
 }
 
 // Funcion Completar fila
-function completarFila(element,index,arr){
-    // Creamos formato para la tabla
-    arr[index] = document.querySelector("#tbl_cliente tbody").innerHTML += 
-    `<tr> 
+function completarFila(element, index, arr) {
+	// Creamos formato para la tabla
+	arr[index] = document.querySelector('#tbl_cliente tbody').innerHTML += `<tr>
     <td>${element.id_cliente}</td>
     <td>${element.dv}</td>
     <td>${element.nombres}</td>
@@ -116,188 +115,207 @@ function completarFila(element,index,arr){
     <a href='actualizar.html?id=${element.id_cliente}' class='btn btn-warning btn-sm'>Actualizar</a>
     <a href='eliminar.html?id=${element.id_cliente}' class='btn btn-danger btn-sm'>Eliminar</a>
     </td>
-    </tr>`
+    </tr>`;
 }
 
 // Funcion para el ID de actualizar el cliente
-function obtenerIdActualizacion(){
-    const queryString = window.location.search;
-    const parametros = new URLSearchParams(queryString);
-    const p_id_cliente = parametros.get('id');
-    g_id_cliente = p_id_cliente;
-    // llamamos a la funcion para obtener datos de actualizacion
-    obtenerDatosActualizacion(p_id_cliente);
+function obtenerIdActualizacion() {
+	const queryString = window.location.search;
+	const parametros = new URLSearchParams(queryString);
+	const p_id_cliente = parametros.get('id');
+	g_id_cliente = p_id_cliente;
+	// llamamos a la funcion para obtener datos de actualizacion
+	obtenerDatosActualizacion(p_id_cliente);
 }
 
 // Funcion para eliminar el id de cliente
-function obtenerIdEliminacion(){
-    const queryString = window.location.search;
-    const parametros = new URLSearchParams(queryString);
-    const p_id_cliente = parametros.get('id');
-    g_id_cliente = p_id_cliente;
-    // llamamos a la funcion para eliminar resultado
-    obtenerDatosEliminacion(p_id_cliente);
+function obtenerIdEliminacion() {
+	const queryString = window.location.search;
+	const parametros = new URLSearchParams(queryString);
+	const p_id_cliente = parametros.get('id');
+	g_id_cliente = p_id_cliente;
+	// llamamos a la funcion para eliminar resultado
+	obtenerDatosEliminacion(p_id_cliente);
 }
 
 // Funcion para obtener datos de eliminacion
-function obtenerDatosEliminacion(id_cliente){
-    // Metodo get para obtener datos de eliminacion
-    const requestOptions = {
-        method: 'GET',
-        redirect: 'follow'
-    };
-    fetch("http://144.126.210.74:8080/api/cliente/"+id_cliente, requestOptions)
-    .then((response) => response.json())
-    .then((json) => json.forEach(completarEtiquetaEliminar))
-    .then((result) => console.log(result))
-    .catch((error) => console.error(error));
+function obtenerDatosEliminacion(id_cliente) {
+	// Metodo get para obtener datos de eliminacion
+	const requestOptions = {
+		method: 'GET',
+		redirect: 'follow'
+	};
+	fetch(
+		'https://crm-react.sbravo.app/api/cliente/' + id_cliente,
+		requestOptions
+	)
+		.then((response) => response.json())
+		.then((json) => json.forEach(completarEtiquetaEliminar))
+		.then((result) => console.log(result))
+		.catch((error) => console.error(error));
 }
 
 // Funcion para etiqueta de eliminacion
-function completarEtiquetaEliminar(element){
-    var nombreCliente = element.nombres;
-    var apellidoCliente = element.apellidos;
-    document.getElementById('lbl_eliminar').innerHTML ="¿Desea eliminar este cliente? <b>"+nombreCliente+" "+apellidoCliente+"</b>";
+function completarEtiquetaEliminar(element) {
+	var nombreCliente = element.nombres;
+	var apellidoCliente = element.apellidos;
+	document.getElementById('lbl_eliminar').innerHTML =
+		'¿Desea eliminar este cliente? <b>' +
+		nombreCliente +
+		' ' +
+		apellidoCliente +
+		'</b>';
 }
 
 // Funcion para obtener datos de actualizacion
-function obtenerDatosActualizacion(id_cliente){
-    // Metodo get para obtener datos de actualizacion
-    const requestOptions = {
-        method: 'GET',
-        redirect: 'follow'
-    };
-    fetch("http://144.126.210.74:8080/api/cliente/"+id_cliente, requestOptions)
-    .then((response) => response.json())
-    .then((json) => json.forEach(completarFormularioActualizar))
-    .then((result) => console.log(result))
-    .catch((error) => console.error(error));
+function obtenerDatosActualizacion(id_cliente) {
+	// Metodo get para obtener datos de actualizacion
+	const requestOptions = {
+		method: 'GET',
+		redirect: 'follow'
+	};
+	fetch(
+		'https://crm-react.sbravo.app/api/cliente/' + id_cliente,
+		requestOptions
+	)
+		.then((response) => response.json())
+		.then((json) => json.forEach(completarFormularioActualizar))
+		.then((result) => console.log(result))
+		.catch((error) => console.error(error));
 }
 
 // Funcion para completar formulario de actualizacion
-function completarFormularioActualizar(element){
-    var dv = element.dv;
-    var nombres = element.nombres;
-    var apellidos = element.apellidos;
-    var email = element.email;
-    var celular = element.celular;
-    document.getElementById('txt_dv').value = dv;
-    document.getElementById('txt_nombre').value = nombres;
-    document.getElementById('txt_apellido').value = apellidos;
-    document.getElementById('txt_email').value = email;
-    document.getElementById('txt_celular').value = celular;
+function completarFormularioActualizar(element) {
+	var dv = element.dv;
+	var nombres = element.nombres;
+	var apellidos = element.apellidos;
+	var email = element.email;
+	var celular = element.celular;
+	document.getElementById('txt_dv').value = dv;
+	document.getElementById('txt_nombre').value = nombres;
+	document.getElementById('txt_apellido').value = apellidos;
+	document.getElementById('txt_email').value = email;
+	document.getElementById('txt_celular').value = celular;
 }
 
 // Funcion actualizar
-function actualizarCliente(){
-    // Variable para obtener nombre del resultado desde interfaz
-    var dv = document.getElementById("txt_dv").value;
-    var nombres = document.getElementById("txt_nombre").value;
-    var apellidos = document.getElementById("txt_apellido").value;
-    var email = document.getElementById("txt_email").value;
-    var celular = document.getElementById("txt_celular").value;
-    // Validaciones
-    // Validar dv
-    const dvValido = /^[0-9kK]$/;
-    if (dv == "") {
-        mostrarAlerta("Error Dv: Campo obligatorio");
-        return;
-    }
-    if (!dvValido.test(dv)) {
-        mostrarAlerta("Error Dv: Número del 0 al 9 o la letra k");
-        return;
-    }
-    // Validar nombres
-    if (nombres == "") {
-        mostrarAlerta("Error Nombres: Campo obligatorio");
-        return;
-    }
-    // Validar apellidos
-    if (apellidos == "") {
-        mostrarAlerta("Error Apellidos: Campo obligatorio");
-        return;
-    }
-    // Validar email
-    if (email == "") {
-        mostrarAlerta("Error Email: Campo obligatorio");
-        return;
-    }
-    // Validar celular
-    if (celular == "") {
-        mostrarAlerta("Error Celular: Campo obligatorio");
-        return;
-    }
-    // Agregar api resultado
-    const myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-    const raw =JSON.stringify({
-        "dv": dv,
-        "nombres": nombres,
-        "apellidos": apellidos,
-        "email": email,
-        "celular": celular
-    });
-    // Metodo patch para actualizar resultado
-    const requestOptions = {
-        method: 'PATCH',
-        headers: myHeaders,
-        body: raw,
-        redirect: 'follow'
-    };
-    fetch("http://144.126.210.74:8080/api/cliente/"+g_id_cliente, requestOptions)
-    .then((response)=>{
-        if(response.status == 200){
-            location.href = "listar.html";
-        }
-        if(response.status == 400){
-            mostrarAlerta("No se pudo actualizar el cliente");
-        }
-    })
-    .then((result) => console.log(result))
-    .catch((error) => console.error(error));
+function actualizarCliente() {
+	// Variable para obtener nombre del resultado desde interfaz
+	var dv = document.getElementById('txt_dv').value;
+	var nombres = document.getElementById('txt_nombre').value;
+	var apellidos = document.getElementById('txt_apellido').value;
+	var email = document.getElementById('txt_email').value;
+	var celular = document.getElementById('txt_celular').value;
+	// Validaciones
+	// Validar dv
+	const dvValido = /^[0-9kK]$/;
+	if (dv == '') {
+		mostrarAlerta('Error Dv: Campo obligatorio');
+		return;
+	}
+	if (!dvValido.test(dv)) {
+		mostrarAlerta('Error Dv: Número del 0 al 9 o la letra k');
+		return;
+	}
+	// Validar nombres
+	if (nombres == '') {
+		mostrarAlerta('Error Nombres: Campo obligatorio');
+		return;
+	}
+	// Validar apellidos
+	if (apellidos == '') {
+		mostrarAlerta('Error Apellidos: Campo obligatorio');
+		return;
+	}
+	// Validar email
+	if (email == '') {
+		mostrarAlerta('Error Email: Campo obligatorio');
+		return;
+	}
+	// Validar celular
+	if (celular == '') {
+		mostrarAlerta('Error Celular: Campo obligatorio');
+		return;
+	}
+	// Agregar api resultado
+	const myHeaders = new Headers();
+	myHeaders.append('Content-Type', 'application/json');
+	const raw = JSON.stringify({
+		dv: dv,
+		nombres: nombres,
+		apellidos: apellidos,
+		email: email,
+		celular: celular
+	});
+	// Metodo patch para actualizar resultado
+	const requestOptions = {
+		method: 'PATCH',
+		headers: myHeaders,
+		body: raw,
+		redirect: 'follow'
+	};
+	fetch(
+		'https://crm-react.sbravo.app/api/cliente/' + g_id_cliente,
+		requestOptions
+	)
+		.then((response) => {
+			if (response.status == 200) {
+				location.href = 'listar.html';
+			}
+			if (response.status == 400) {
+				mostrarAlerta('No se pudo actualizar el cliente');
+			}
+		})
+		.then((result) => console.log(result))
+		.catch((error) => console.error(error));
 }
 
 // Funcion para eliminar resultado
-function eliminarCliente(){
-    // Agregamos api resultado
-    const myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-    // Metodo delete para eliminar resultado
-    const requestOptions = {
-        method: 'DELETE',
-        headers: myHeaders,
-        redirect: 'follow'
-    };
-    fetch("http://144.126.210.74:8080/api/cliente/"+g_id_cliente, requestOptions)
-    .then((response)=>{
-        if(response.status == 200){
-            location.href = "listar.html";
-        }
-        if(response.status == 400){
-            mostrarAlerta("No se puede eliminar el cliente")
-        }
-    })
-    .then((result) => console.log(result))
-    .catch((error) => console.error(error));
+function eliminarCliente() {
+	// Agregamos api resultado
+	const myHeaders = new Headers();
+	myHeaders.append('Content-Type', 'application/json');
+	// Metodo delete para eliminar resultado
+	const requestOptions = {
+		method: 'DELETE',
+		headers: myHeaders,
+		redirect: 'follow'
+	};
+	fetch(
+		'https://crm-react.sbravo.app/api/cliente/' + g_id_cliente,
+		requestOptions
+	)
+		.then((response) => {
+			if (response.status == 200) {
+				location.href = 'listar.html';
+			}
+			if (response.status == 400) {
+				mostrarAlerta('No se puede eliminar el cliente');
+			}
+		})
+		.then((result) => console.log(result))
+		.catch((error) => console.error(error));
 }
 
 // Funcion para formatear la fecha y hora (la que necesita el backend)
-function obtenerFechaHora(){
-    var fechaHoraActual = new Date();
-    var fechaFormateada = fechaHoraActual.toLocaleDateString('es-ES',{
-        hour12: false,
-        year: 'numeric',
-        month: '2-digit',
-        day: '2-digit',
-        hour: '2-digit',
-        minute: '2-digit',
-        second: '2-digit'
-    }).replace(/(\d+)\/(\d+)\/(\d+)\,\s*(\d+):(\d+):(\d+)/,'$3-$2-$1 $4:$5:$6');
-    return fechaFormateada;
+function obtenerFechaHora() {
+	var fechaHoraActual = new Date();
+	var fechaFormateada = fechaHoraActual
+		.toLocaleDateString('es-ES', {
+			hour12: false,
+			year: 'numeric',
+			month: '2-digit',
+			day: '2-digit',
+			hour: '2-digit',
+			minute: '2-digit',
+			second: '2-digit'
+		})
+		.replace(/(\d+)\/(\d+)\/(\d+)\,\s*(\d+):(\d+):(\d+)/, '$3-$2-$1 $4:$5:$6');
+	return fechaFormateada;
 }
 
 // Funcion para mostrar alerta en caso de error
-function mostrarAlerta(mensaje){
-    const alertContainer = document.getElementById('alert-container');
-    alertContainer.innerHTML = `<div class="alert alert-danger alert-dismissible fade show" role="alert">${mensaje}<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>`;
+function mostrarAlerta(mensaje) {
+	const alertContainer = document.getElementById('alert-container');
+	alertContainer.innerHTML = `<div class="alert alert-danger alert-dismissible fade show" role="alert">${mensaje}<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>`;
 }
